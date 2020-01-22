2078188e71dc867be72207ec986e5cc45b2ec502
Chineek Favors <chineekfavors01@gmail.com>
2020-01-21 21:50:18 -0800

    NPM installed helmet

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const server = express();
+const helmet = require('helmet');
 const userRouter = require('./users/userRouter.js');
 
-
+server.use(helmet());
 server.use(express.json());
+
 server.use('/', userRouter);
 
 
@@ -12,6 +14,8 @@ server.use('/', userRouter);
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+
+}
 
 module.exports = server;
